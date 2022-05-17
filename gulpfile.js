fd88cdbed4baa688a62b0db1a14162f573ea0f14
Vaibhav Pathak <vaibhavpathak852947@gmail.com>
2022-05-17 23:20:49 +0530

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -8,58 +8,58 @@ const imgaemin = require('gulp-imagemin');
 const del = require('del');
 
 
-gulp.task('css',function (done) {
+gulp.task('css', function (done) {
     console.log('minifying css...');
     gulp.src('./assets/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(cssnano())
-    .pipe(gulp.dest('./assets/css'));
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(gulp.dest('./assets/css'));
 
     gulp.src('./assets/**/*.css')
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd:'public',
-        merge:true
-    }))
-    .pipe(gulp.dest('./public/assets'));
+        .pipe(rev())
+        .pipe(gulp.dest('./public/assets'))
+        .pipe(rev.manifest({
+            cwd: 'public',
+            merge: true
+        }))
+        .pipe(gulp.dest('./public/assets'));
     done();
 });
 
-gulp.task('js',function (done) {
+gulp.task('js', function (done) {
     console.log('minfying js...');
     gulp.src('./assets/**/*.js')
-    .pipe(uglify())
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
+        .pipe(uglify())
+        .pipe(rev())
+        .pipe(gulp.dest('./public/assets'))
+        .pipe(rev.manifest({
+            cwd: 'public',
+            merge: true
+        }))
+        .pipe(gulp.dest('./public/assets'));
     done();
 });
 
-gulp.task('images',function (done) {
+gulp.task('images', function (done) {
     console.log('compressing images...');
     gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
-    .pipe(imgaemin())
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd:'public',
-        merge: true,
-    }))
-    .pipe(gulp.dest('./public/assets'));
+        .pipe(imgaemin())
+        .pipe(rev())
+        .pipe(gulp.dest('./public/assets'))
+        .pipe(rev.manifest({
+            cwd: 'public',
+            merge: true,
+        }))
+        .pipe(gulp.dest('./public/assets'));
     done();
 });
 
-gulp.task('clean:assets',function (done) {
+gulp.task('clean:assets', function (done) {
     del.sync('./public/assets');
     done();
 })
 
-gulp.task('build',gulp.series('clean:assets','css','js','images'),function (done) {
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function (done) {
     console.log('Building assets');
     done();
 })
